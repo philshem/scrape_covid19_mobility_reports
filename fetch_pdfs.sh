70856ff0f301445528ce5b7e7ca63e94798c1761
Philip Shemella <philip@skunksports.com>
2020-04-05 09:27:05 +0200

    add source to fetching script

--- fetch_pdfs.sh
+++ fetch_pdfs.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# adapted from https://github.com/patrikpolyak/google_covid19_community_mobility_reports_data/blob/master/fetch_pdfs.sh
+
 pdf_dir="pdf"
 site_data=$(curl -s 'https://www.google.com/covid19/mobility/')
 [ ! -d "$pdf_dir" ] && mkdir -p "$pdf_dir"
